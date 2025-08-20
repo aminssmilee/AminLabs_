5531a7b34c8c0bdeee1ff8c71f7c31017456f67a
aminssmilee <salisahmad48@gmail.com>
2025-08-20 19:55:57 +0700

    FIx banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,24 +9,39 @@ export default function Banner() {
     //   }}
     // >
     <div
-      className="relative w-full h-[500px] md:h-screen bg-cover bg-center"
+      className="relative w-full h-[500px] min-h-screen bg-cover bg-center font-helvetica"
       style={{
         backgroundImage: `url('${banner}')`,
       }}
     >
       {/* Text */}
-      <div className="absolute top-1/4 left-4 md:left-8 text-white">
-        <h2 className="text-2xl md:text-4xl font-bold leading-tight">NEW</h2>
-        <h2 className="text-2xl md:text-4xl font-bold leading-tight">COLLECTION</h2>
-        <p className="text-sm md:text-lg mt-2">SPRING 2024</p>
+      <div
+        className="
+    absolute 
+    left-1/2 
+    transform -translate-x-1/2 -translate-y-1/2 
+    text-center text-white
+    md:top-1/4 top-56 md:left-8 md:transform-none md:text-left
+    tracking-tight
+  "
+      >
+        <h2 className="text-5xl md:text-7xl font-semibold text-start leading-none m-0">NEW</h2>
+        <h2 className="text-5xl md:text-7xl font-semibold leading-none m-0">COLLECTION</h2>
+        <p className="text-lg md:text-2xl text-end leading-none m-0">SPRING 2024</p>
       </div>
 
+
+
+
+
+
       {/* Button */}
-      <div className="absolute bottom-10 md:bottom-20 right-1/2 translate-x-1/2 md:right-20 md:translate-x-0">
-        <button className="bg-beig text-black px-20 md:px-36 py-2 md:py-3 border-2 border-black rounded-sm shadow hover:bg-opacity-80 transition">
+      <div className="absolute bottom-56 md:bottom-20 right-0 left-0 md:right-20 md:left-auto flex justify-center md:justify-end">
+        <button className="bg-beig text-2xl w-80 md:w-auto text-dark px-6 md:px-36 py-1 md:py-3  hover:bg-opacity-80 transition">
           SHOP NOW
         </button>
       </div>
+
     </div>
   );
 }
