c4e40d433ba2be968c630786f7fcd5288b0ad973
aminssmilee <salisahmad48@gmail.com>
2025-08-21 18:21:04 +0700

    Fix SPA routing

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,6 @@ import LookBook from "./components/LookBook";
 
 import CatalogPage from "./pages/CatalogPage";
 import ClothesPage from "./pages/ClothesPage";
-// import ShoesPage from "./pages/ShoesPage";
-// import BagsPage from "./pages/BagsPage";
-// import AccessoriesPage from "./pages/AccessoriesPage";
 
 function App() {
   return (
@@ -32,11 +29,8 @@ function App() {
         />
 
         {/* Halaman katalog & kategori */}
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/catalog/clothes" element={<ClothesPage />} />
-        {/* <Route path="home/catalog/shoes" element={<ShoesPage />} />
-        <Route path="home/catalog/bags" element={<BagsPage />} />
-        <Route path="home/catalog/accessories" element={<AccessoriesPage />} /> */}
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="catalog/clothes" element={<ClothesPage />} />
       </Route>
     </Routes>
   );
